e08797c195a574831789706be75fec9fc4d25af8
kzhunter <0xkzhunter@gmail.com>
2023-06-30 14:02:11 +0530

    d5-separated math util functions

---
+++ src/util/Math.py
@@ -0,0 +1,17 @@
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = extended_gcd(b % a, a)
+        return gcd, y - (b // a) * x, x
+
+def mod_inverse(a, m):
+    gcd, x, y = extended_gcd(a, m)
+    if gcd != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % m
+
+def is_coprime(a, m):
+    gcd, _, _ = extended_gcd(a, m)
+    return gcd == 1        
